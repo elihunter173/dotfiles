c97e01d651358e86afd80d8b5233c1f218a0d9ab
Eli W. Hunter <hunter.eli.w@gmail.com>
2018-11-25 09:02:50 -0500

    Create 'lvim'

--- zshrc.d/03.aliases.sh
+++ zshrc.d/03.aliases.sh
@@ -1,7 +1,12 @@
 # `command -v` prints out the version of `command`. This can be used to check if something exists.
 # /dev/null is a standard dumping ground for stdout. 2>&1 reroutes stderr to stdout.
 
-# Sets vim to start neovim if vim isn't installed. Neovim is better anyway ;)
+# enable listing all dot files
+alias l.='ls -d .*'
+
+# set vim to neovim if vim isn't installed. it's bettery anyway ;)
 vim --version >/dev/null 2>&1 || alias vim='nvim'
 
-alias l.='ls -d .*'
+# enable 'Light Vim'
+alias lvim=`nvim -u ~/.lvimrc'
+
