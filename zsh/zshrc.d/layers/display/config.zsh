48211b4df388f36b6f7fa4d7357003714a7bf267
Eli W. Hunter <hunter.eli.w@gmail.com>
2018-12-21 18:41:02 -0500

    Add BASE16 config to zsh

---
+++ zsh/zshrc.d/layers/display/config.zsh
@@ -0,0 +1,6 @@
+# For BASE16 Config
+# Base16 Shell
+BASE16_SHELL="$HOME/.config/base16-shell/"
+[ -n "$PS1" ] && \
+    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+        eval "$("$BASE16_SHELL/profile_helper.sh")"
