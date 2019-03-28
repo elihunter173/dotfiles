7c651bcb5454a88b8defcf05282f93d42058cea3
Eli W. Hunter <hunter.eli.w@gmail.com>
2019-03-28 15:00:46 -0400

    Make all standard configs POSIX compliant

---
+++ bookmarks.sh
@@ -0,0 +1,10 @@
+# Misc
+export TRASH="$HOME/.local/share/Trash/files"
+
+# Programming
+export ARC="$SRC/arc"
+
+# Notes
+export NOTES="$HOME/Documents/notes"
+export csc_notes="$NOTES/2019-Spring/CSC-216"
+export csc_src="$SRC/coursework/CSC-216"
