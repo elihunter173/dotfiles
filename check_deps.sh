bee2b589f25fc82864b4f2f2dd57b867c4732215
Eli W. Hunter <elihunter173@gmail.com>
2020-05-09 14:40:15 -0400

    Add more dependencies to check_deps.sh

--- check_deps.sh
+++ check_deps.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-# TODO: Use different colors for different meanings. For example, have
-# check_for display messages in a warning color
-
 # Set up so we always print our desired color
 WARN="\033[31m"
 OKAY="\033[34m"
@@ -23,32 +20,37 @@ try_install() {
   # assign it to a variable because that causes issues with string
   # separation.
   shift
+
   if command -v "$bin" > /dev/null; then
     printf "$OKAY"
     echo "'$bin' already installed. Skipping..."
     return
   fi
 
+  # We don't have it installed, so we loop over and installation commands,
+  # exiting with the first one that was agreed to and worked.
   for cmd in "$@"; do
     printf "$OKAY"
     printf "Install '$bin' with '$cmd'? (y/N) "
     read response
-    case "$response" in
-      ([Yy])
-        if eval "$cmd"; then
-          return
-        else
-          printf "$WARN"
-          echo "'$cmd' failed!"
-          break
-        fi
-        ;;
-    esac
+    if [ "$response" = "y" ] || [ "$response" = "Y" ]; then
+      if eval "$cmd"; then
+        return
+      else
+        # The command we wanted to try failed, so let's quit early
+        printf "$WARN"
+        echo "'$cmd' failed!"
+        break
+      fi
+    fi
   done
 
   printf "$WARN"
   echo "'$bin' couldn't be found and is recommended!"
 }
 
+try_install 'awesome'
 try_install 'direnv'
 try_install 'nvim'
+try_install 'rofi'
+try_install 'xautolock'
