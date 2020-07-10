fa76b80f0b0ff134be04fac3b4a8134bd9e804d2
Eli W. Hunter <elihunter173@gmail.com>
2020-07-10 15:11:03 -0400

    Remove unused bits

--- etc/export.sh
+++
@@ -1,9 +0,0 @@
-#/usr/bin/env sh
-
-# Check if the current user is root
-if [ $(id --user) -ne 0 ]; then
-    echo "This script must be run as root."
-    exit 1
-fi
-cd $(dirname $0)
-cp -r . /etc/.
