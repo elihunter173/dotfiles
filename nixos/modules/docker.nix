55e183742bac07e12ae17a60d75a8a6396c63e4a
Eli W. Hunter <elihunter173@gmail.com>
2020-07-25 15:48:15 -0400

    [nixos] Remove task limit from docker

--- nixos/modules/docker.nix
+++ nixos/modules/docker.nix
@@ -7,5 +7,10 @@
     environment.systemPackages = with pkgs; [
       docker-compose
     ];
+    # By default we're limited to around ~5000 tasks, which means you can only
+    # have around 200 concurrent containers. This poses an issue for Codie if
+    # she is receiving hundreds of requests in a short amount of time.
+    systemd.services.docker-cor.serviceConfig.TasksMax = "infinity";
+    systemd.services.docker.serviceConfig.TasksMax = "infinity";
   };
 }
