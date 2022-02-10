cdedd150b3136adb9049e1440c9c0d59334bb457
Eli W. Hunter <elihunter173@gmail.com>
2022-02-10 12:24:20 -0500

    [nvim] Swap from formatter.nvim to null-ls

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,6 @@
 local cmd, g, opt = vim.cmd, vim.g, vim.opt
 
-local MAP_DEFAULTS = {noremap = true}
+local MAP_DEFAULTS = { noremap = true }
 local function map(mode, lhs, rhs, opts)
   opts = vim.tbl_extend("force", MAP_DEFAULTS, opts or {})
   vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
@@ -9,53 +9,55 @@ end
 -----------------------------
 ---------- Plugins ----------
 -----------------------------
-local install_path = vim.fn.stdpath("data") ..
-                         "/site/pack/packer/start/packer.nvim"
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 local packer_bootstrap = false
 if vim.fn.isdirectory(install_path) == 0 then
   vim.fn.system {
-    "git", "clone", "--depth=1", "https://github.com/wbthomason/packer.nvim",
+    "git",
+    "clone",
+    "--depth=1",
+    "https://github.com/wbthomason/packer.nvim",
     install_path,
   }
   packer_bootstrap = true
 end
 require("packer").startup(function(use)
-  use "wbthomason/packer.nvim"
+  use("wbthomason/packer.nvim")
 
-  use "lifepillar/vim-gruvbox8"
+  use("lifepillar/vim-gruvbox8")
 
   -- Custom filetypes
-  use "chr4/nginx.vim"
-  use "JuliaEditorSupport/julia-vim"
-  use "plasticboy/vim-markdown"
+  use("chr4/nginx.vim")
+  use("JuliaEditorSupport/julia-vim")
+  use("plasticboy/vim-markdown")
   -- For :TableFormat in markdown
-  use "godlygeek/tabular"
+  use("godlygeek/tabular")
 
   -- General editing
   -- Easier commenting for any language
-  use "tpope/vim-commentary"
+  use("tpope/vim-commentary")
   -- Additional text objects
-  use "wellle/targets.vim"
+  use("wellle/targets.vim")
   -- Surrounding text objects with any character
-  use "machakann/vim-sandwich"
+  use("machakann/vim-sandwich")
   -- Vim undotree visualizer
-  use "mbbill/undotree"
+  use("mbbill/undotree")
   -- Never think about indentation
-  use "tpope/vim-sleuth"
+  use("tpope/vim-sleuth")
   -- https://EditorConfig.org
-  use "editorconfig/editorconfig-vim"
+  use("editorconfig/editorconfig-vim")
   -- Git
-  use "tpope/vim-fugitive"
+  use("tpope/vim-fugitive")
 
   -- Multi-cursor
-  use "mg979/vim-visual-multi"
+  use("mg979/vim-visual-multi")
 
   -- The file manager I made. I normally just symlink it
   -- use "elihunter173/dirbuf.nvim"
 
   -- Floating terminal
-  use "voldikss/vim-floaterm"
+  use("voldikss/vim-floaterm")
 
   -- Fuzzy finding
   use {
@@ -67,27 +69,27 @@ require("packer").startup(function(use)
   }
 
   -- Zettelkasten notes
-  use "mickael-menu/zk-nvim"
+  use("mickael-menu/zk-nvim")
 
   -- LSP, snippets, and autocomplete
-  use "neovim/nvim-lspconfig"
-  use "L3MON4D3/LuaSnip"
+  use("neovim/nvim-lspconfig")
+  use("L3MON4D3/LuaSnip")
   -- TODO: Check out coq.nvim?
-  use {"hrsh7th/nvim-cmp", "hrsh7th/cmp-nvim-lsp", "saadparwaiz1/cmp_luasnip"}
+  use { "hrsh7th/nvim-cmp", "hrsh7th/cmp-nvim-lsp", "saadparwaiz1/cmp_luasnip" }
+
+  -- LSP integration for generic things (e.g. formatters)
+  use { "jose-elias-alvarez/null-ls.nvim", requires = "nvim-lua/plenary.nvim" }
 
   -- TreeSitter
   use {
     "nvim-treesitter/nvim-treesitter",
     "nvim-treesitter/nvim-treesitter-refactor",
   }
-
-  -- Formatting
-  use "mhartington/formatter.nvim"
 end)
 
 if packer_bootstrap then
   require("packer").sync()
-  cmd "autocmd User PackerComplete echo 'Initial bootstrap done. Run :quitall and restart'"
+  cmd("autocmd User PackerComplete echo 'Initial bootstrap done. Run :quitall and restart'")
   return
 end
 
@@ -125,7 +127,7 @@ opt.title = true
 -- Don't redraw during macros and more (for performance)
 opt.lazyredraw = true
 -- Better completion experience
-opt.completeopt = {"menu", "menuone", "noselect"}
+opt.completeopt = { "menu", "menuone", "noselect" }
 opt.shortmess:append("c")
 -- Persistent undo
 opt.undofile = true
@@ -157,15 +159,15 @@ end
 opt.background = "dark"
 
 -- Automatically enable spelling on certain files
-cmd "autocmd FileType tex,markdown setlocal spell"
+cmd("autocmd FileType tex,markdown setlocal spell")
 -- Use single-line comments for C and C++
-cmd "autocmd FileType c,cpp set commentstring=//%s"
+cmd("autocmd FileType c,cpp set commentstring=//%s")
 
 -----------------------------
 ---------- Mappings ----------
 -----------------------------
 -- TODO: See if theres a Lua native way to do this
-cmd "let mapleader = ' '"
+cmd("let mapleader = ' '")
 map("n", "<leader>w", "<cmd>write<cr>")
 -- Make leader keybindings less awkward
 map("n", " ", "")
@@ -173,10 +175,10 @@ map("n", " ", "")
 map("n", "s", "")
 
 -- Send c to black hole
-map("n", "c", "\"_c")
-map("n", "C", "\"_C")
-map("v", "c", "\"_c")
-map("v", "C", "\"_C")
+map("n", "c", '"_c')
+map("n", "C", '"_C')
+map("v", "c", '"_c')
+map("v", "C", '"_C')
 
 -- Turn off search highlighting because vim doesn't do that by default for some
 -- reason
@@ -203,24 +205,26 @@ map("t", "<C-k>", "<C-\\><C-n><C-w>k")
 map("t", "<C-l>", "<C-\\><C-n><C-w>l")
 -- Make terminals always open in insert mode with no linenumbers.
 -- TODO: Use autocmd API when it gets finished
-cmd "autocmd TermOpen * startinsert | setlocal norelativenumber nonumber"
+cmd("autocmd TermOpen * startinsert | setlocal norelativenumber nonumber")
 
 -- Add todo comments
 -- TODO: Make this less hacky
-map("n", "<leader>c",
-    "<cmd>call append('.', substitute(&commentstring, '\\s*%s\\s*', ' TODO: ', ''))<cr>j==f:la")
-map("n", "<leader>C",
-    "<cmd>call append(line('.')-1, substitute(&commentstring, '\\s*%s\\s*', ' TODO: ', ''))<cr>k==f:la")
+map("n", "<leader>c", "<cmd>call append('.', substitute(&commentstring, '\\s*%s\\s*', ' TODO: ', ''))<cr>j==f:la")
+map(
+  "n",
+  "<leader>C",
+  "<cmd>call append(line('.')-1, substitute(&commentstring, '\\s*%s\\s*', ' TODO: ', ''))<cr>k==f:la"
+)
 
 -----------------------------
 ------- Plugin Config -------
 -----------------------------
 -- Colorscheme
 g.gruvbox_italicize_strings = 0
-cmd "colorscheme gruvbox8"
+cmd("colorscheme gruvbox8")
 
 -- EditorConfig + Fugitive
-g.EditorConfig_exclude_patterns = {"fugitive://.\\*"}
+g.EditorConfig_exclude_patterns = { "fugitive://.\\*" }
 
 -- Fugitive
 map("n", "<leader>gs", "<cmd>Git<cr>")
@@ -242,19 +246,19 @@ cmd "autocmd FileType markdown set textwidth=80"
 
 -- Multi-cursor
 g.VM_leader = "\\"
-g.VM_maps = {["Add Cursor Down"] = "<M-j>", ["Add Cursor Up"] = "<M-k>"}
+g.VM_maps = { ["Add Cursor Down"] = "<M-j>", ["Add Cursor Up"] = "<M-k>" }
 
 -- Don't open unnecessary files
 g.fzf_buffers_jump = 1
-g.fzf_layout = {window = {width = 0.85, height = 0.8}}
+g.fzf_layout = { window = { width = 0.85, height = 0.8 } }
 g.fzf_preview_window = ""
 -- Nice fzf keybindings
-cmd [[
+cmd([[
 command! -bang -nargs=? -complete=dir Directories
   \ call fzf#run(fzf#wrap({'source': 'fd --type directory' . ('<bang>' == '!' ? ' --hidden' : '')}))
 command! -bang -nargs=? -complete=dir Files
   \ call fzf#run(fzf#wrap({'source': 'fd --type file' . ('<bang>' == '!' ? ' --hidden' : '')}))
-]]
+]])
 map("n", "<leader>o", "<cmd>Files<cr>")
 map("n", "<leader>O", "<cmd>Files!<cr>")
 map("n", "<leader>p", "<cmd>Directories<cr>")
@@ -268,74 +272,37 @@ g.floaterm_width = 0.8
 g.floaterm_height = 0.8
 
 --------------------------------
----------- Formatting ----------
---------------------------------
--- TODO: Remove all command line configuration in favor of config files?
-local prettier = function()
-  return {
-    exe = "prettier",
-    args = {
-      "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--trailing-comma=all",
-      "--print-width=100",
-    },
-    stdin = true,
-  }
-end
-
-local function fmt_call(exe, ...)
-  local tbl = {exe = exe, args = {...}, stdin = true}
-  return function()
-    return tbl
-  end
-end
-
-require("formatter").setup {
-  logging = false,
-  filetype = {
-    html = {prettier},
-    css = {prettier},
-    scss = {prettier},
-    javascript = {prettier},
-    typescript = {prettier},
-    vue = {prettier},
-    json = {prettier},
-    lua = {
-      fmt_call("lua-format", "--indent-width=2", "--extra-sep-at-table-end",
-               "--no-keep-simple-control-block-one-line",
-               "--no-keep-simple-function-one-line",
-               "--single-quote-to-double-quote"),
-    },
-    python = {fmt_call("black", "-"), fmt_call("isort", "-")},
-    rust = {fmt_call("rustfmt", "--edition=2018", "--emit=stdout")},
-    c = {fmt_call("clang-format")},
-    cpp = {fmt_call("clang-format")},
-    go = {fmt_call("gofmt")},
-  },
-}
-map("n", "<leader>f", "<cmd>Format<cr>")
-
---------------------------------
 ---------- Snippets ------------
 --------------------------------
 local luasnip = require("luasnip")
-local env_snippet = luasnip.parser.parse_snippet("env", [[
+local env_snippet = luasnip.parser.parse_snippet(
+  "env",
+  [[
 \begin{$1}
   $0
-\end{$1}]])
+\end{$1}]]
+)
 luasnip.snippets = {
-  tex = {env_snippet},
-  markdown = {env_snippet},
+  tex = { env_snippet },
+  markdown = { env_snippet },
   rust = {
-    luasnip.parser.parse_snippet("macro", [[
+    luasnip.parser.parse_snippet(
+      "macro",
+      [[
 macro_rules! $1 {
   ($2) => {
     $0
   };
-}]]), luasnip.parser.parse_snippet("test", [[
+}]]
+    ),
+    luasnip.parser.parse_snippet(
+      "test",
+      [[
 #[test]
 fn test_$1() {
   $0
-}]]),
+}]]
+    ),
   },
 }
 
@@ -360,42 +327,49 @@ cmp.setup {
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm({select = true}),
+    ["<CR>"] = cmp.mapping.confirm { select = true },
   },
-  sources = {{name = "nvim_lsp"}, {name = "luasnip"}},
+  sources = { { name = "nvim_lsp" }, { name = "luasnip" } },
 }
+
+map("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>")
+map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+map("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>")
 
 local function lsp_attach(_, bufnr)
   local function bufmap(mode, lhs, rhs)
-    local opts = vim.tbl_extend("force", MAP_DEFAULTS, {silent = true})
+    local opts = vim.tbl_extend("force", MAP_DEFAULTS, { silent = true })
     vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
   end
 
-  bufmap("n", "gd", "<cmd>lua vim.lsp.buf.declaration()<cr>")
-  bufmap("n", "<c-]>", "<cmd>lua vim.lsp.buf.definition()<cr>")
-  bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
-  bufmap("n", "gD", "<cmd>lua vim.lsp.buf.implementation()<cr>")
-  bufmap("n", "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
-  bufmap("n", "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
-  bufmap("n", "1gD", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
-  bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>")
-  bufmap("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<cr>")
-  bufmap("n", "g0", "<cmd>lua vim.lsp.buf.document_symbol()<cr>")
-  bufmap("n", "gW", "<cmd>lua vim.lsp.buf.workspace_symbol()<cr>")
-  bufmap("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<cr>")
+  -- Recommended settings
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+  bufmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+  bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+  bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+  bufmap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+  bufmap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+  -- This conflicts with my :write binding
+  -- bufmap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>")
+  -- bufmap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>")
+  -- bufmap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>")
+  bufmap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+  bufmap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+  bufmap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+  bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+  bufmap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+
+  -- My settings
   bufmap("n", "ge", "<cmd>lua vim.diagnostic.show_line_diagnostics()<cr>")
-  bufmap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
-  bufmap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
 
   print("LSP Attached.")
 end
 
 local lspconfig = require("lspconfig")
-lspconfig.util.default_config = vim.tbl_extend("force",
-                                               lspconfig.util.default_config, {
+lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
   on_attach = lsp_attach,
-  capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol
-                                                                 .make_client_capabilities()),
+  capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
 })
 
 lspconfig.pylsp.setup {}
@@ -404,17 +378,22 @@ lspconfig.tsserver.setup {}
 lspconfig.vimls.setup {}
 lspconfig.sumneko_lua.setup {
   cmd = {
-    os.getenv("HOME") .. "/bin/lua-language-server", "-E",
+    os.getenv("HOME") .. "/bin/lua-language-server",
+    "-E",
     os.getenv("HOME") .. "/src/build/lua-language-server/main.lua",
   },
   settings = {
     Lua = {
-      runtime = {version = "LuaJIT"},
+      runtime = { version = "LuaJIT" },
       diagnostics = {
         globals = {
           "vim", -- neovim
-          "awesome", "client", "screen", -- awesome
-          "describe", "it", "pending", -- busted
+          "awesome",
+          "client",
+          "screen", -- awesome
+          "describe",
+          "it",
+          "pending", -- busted
         },
       },
     },
@@ -427,9 +406,24 @@ lspconfig.clangd.setup {}
 lspconfig.rust_analyzer.setup {}
 
 --------------------------------
+----------- Null-ls ------------
+--------------------------------
+local null_ls = require("null-ls")
+null_ls.setup {
+  sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.gofmt,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.rustfmt,
+    null_ls.builtins.formatting.clang_format,
+  },
+}
+
+--------------------------------
 ------ Zettelkasten notes ------
 --------------------------------
-require("zk").setup {picker = "fzf", lsp = {config = {on_attach = lsp_attach}}}
+require("zk").setup { picker = "fzf", lsp = { config = { on_attach = lsp_attach } } }
 
 require("zk.commands").add("ZkEdit", function()
   require("zk").edit({}, {
@@ -444,11 +438,11 @@ require("zk.commands").add("ZkLink", function(options)
   if options == nil then
     options = {}
   end
-  local picker_options = {title = "Zk Insert Link"}
+  local picker_options = { title = "Zk Insert Link" }
   require("zk").pick_notes(options, picker_options, function(notes)
     for _, note in ipairs(notes) do
       local link = "[" .. note.title .. "](" .. note.path .. ")"
-      vim.api.nvim_put({link}, "c", true, true)
+      vim.api.nvim_put({ link }, "c", true, true)
     end
   end)
 end)
@@ -460,7 +454,7 @@ require("zk.commands").add("ZkLinkSelection", function(options)
   local range = require("zk.util").get_selected_range()
   local text = require("zk.util").get_text_in_range(range)
   assert(text ~= nil, "No selected text")
-  options = vim.tbl_extend("force", {match = text}, options or {})
+  options = vim.tbl_extend("force", { match = text }, options or {})
   local picker_options = {
     title = "Zk Insert Link matching " .. vim.inspect(text),
   }
@@ -469,16 +463,22 @@ require("zk.commands").add("ZkLinkSelection", function(options)
     assert(#notes == 1, "Only select one note")
     local note = notes[1]
     local link = "[" .. text .. "](" .. note.path .. ")"
-    vim.api.nvim_buf_set_text(0, range.start.line, range.start.character,
-                              range["end"].line, range["end"].character, {link})
+    vim.api.nvim_buf_set_text(
+      0,
+      range.start.line,
+      range.start.character,
+      range["end"].line,
+      range["end"].character,
+      { link }
+    )
   end)
-end, {needs_selection = true})
+end, { needs_selection = true })
 
 map("n", "<leader>n", "<cmd>ZkEdit<cr>")
 map("v", "<leader>n", "<cmd>'<,'>ZkNewFromTitleSelection<cr>")
 map("n", "<leader>m", "<cmd>ZkLink<cr>")
 map("v", "<leader>m", ":ZkLinkSelection<cr>")
-cmd "command! ZkUpdate !zk update"
+cmd("command! ZkUpdate !zk update")
 
 --------------------------------
 ---------- TreeSitter ----------
@@ -492,6 +492,9 @@ require("nvim-treesitter.configs").setup {
   },
 }
 -- This is kinda illegal. I took this from the CursorHold autocmd
-map("n", "S",
-    "<cmd>lua require'nvim-treesitter-refactor.highlight_definitions'.highlight_usages(vim.fn.bufnr())<cr>",
-    {silent = true})
+map(
+  "n",
+  "S",
+  "<cmd>lua require'nvim-treesitter-refactor.highlight_definitions'.highlight_usages(vim.fn.bufnr())<cr>",
+  { silent = true }
+)
